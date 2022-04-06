98e76ec6fa77855e53f8b2e0badf88bb5e30c9f8
DiMatveev1991 <98545906+DiMatveev1991@users.noreply.github.com>
2022-04-06 10:51:45 +0300

    dbdfb

---
+++ 9-1/MyException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_1
+{
+    internal class MyException: FileNotFoundException
+    {
+public MyException ()
+        {
+
+        }
+        public MyException (string message) : base(message)
+        { }
+    }
+}
---
+++ 9-1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _9_1;
+
+class Program
+{
+    static void Main (string [] args)
+    {
+        Exception[] exception = { new PlatformNotSupportedException(), new ArgumentNullException(), new ArgumentOutOfRangeException(), new FormatException(), new MyException () };
+
+        foreach (Exception e in exception)
+        {
+            try
+            {
+                throw e;
+            }
+            catch (PlatformNotSupportedException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (MyException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+
+    }
+
+}
